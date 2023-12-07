bd41399f1bfe590f45a9cb0e28d26556de48c7e9
crishard <crislantorres9@gmail.com>
2023-12-07 10:30:07 -0300

    settings: injeção de dependênicas

---
+++ MediFlow/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        "background": "",
+        "text": "#F2F2F2",
+        "primary": "#3889F2",
+        "secondary": "#393940"
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
